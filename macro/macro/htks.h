cd93f07ca0b7db350ba53156f4d4b3de12e9a28e
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2023-12-15 23:07:01 -0300

    asdkop

--- macro/macro/htks.h
+++ macro/macro/htks.h
@@ -7,13 +7,27 @@
 
 class Hotkey {
 public:
+	int atv;
+	bool ctr = false;
 	void InputHK(std::vector<BYTE> htks);
-	void Definehtks();
-private:
-
 };
 
+
 void Hotkey::InputHK(std::vector<BYTE> htks) {
+	std::cout << "\ndigite a tecla que voce usara para ativacao do seu macro: \n" << std::endl;
+	std::cin >> atv;
+	std::cout << "\npronto macro configurado" << std::endl;
+	while (!ctr) {
+		if (GetAsyncKeyState(atv)) {
+			for (int i = 0; i < htks.size(); i++) {
+				keybd_event(htks[i], 0, KEYEVENTF_EXTENDEDKEY, 0);
+				Sleep(50);
+				keybd_event(htks[i], 0, KEYEVENTF_KEYUP, 0);
+			}
+		}
+		Sleep(50);
+	}
 	
 }
+
 #endif
--- macro/macro/macro.cpp
+++ macro/macro/macro.cpp
@@ -11,8 +11,9 @@ int main()
     do {
         std::cout << "*-*-*-*-**-*-*-*-**-*-*-*-**-*-*-*-**-*-*-*-**-*-*-*-*" << std::endl;
         std::cout << "Bem vindo ao MacroCLKS\n" << std::endl;
-        std::cout << "\nDigite 1 para o macro de clicks" << std::endl;
-        std::cout << "\nDigite 2 para o macro de hotkeys" << std::endl;
+        std::cout << "\nDigite 1 para o macro de clicks: " << std::endl;
+        std::cout << "\nDigite 2 para o macro de hotkeys: " << std::endl;
+        std::cout << "\nDigite (1) ou (2)" << std::endl;
         std::cout << "*-*-*-*-**-*-*-*-**-*-*-*-**-*-*-*-**-*-*-*-**-*-*-*-*" << std::endl;
         std::cin >> choice;
 
@@ -22,6 +23,34 @@ int main()
         }
         else if (choice == 2) {
             Hotkey* h1 = new Hotkey();
+            bool controle = false;
+            int keys;
+            std::vector<BYTE> tcl;
+
+            std::cout << "digite a tecla que voce quer adicionar no macro em hexadecimal exemplo: (0x48)" << std::endl;
+            std::cout << "\nDigite 0 para sair" << std::endl;
+
+            while (!controle) {
+                Sleep(50);
+                std::cout << "\nqual tecla quer adicionar?: " << std::endl;
+                std::cin >> std::hex >> keys;
+                
+                if (keys==0) {
+                    controle = true;
+                }
+                else {
+                    tcl.push_back(keys);
+                }
+
+            }
+            std::cout << keys << std::endl;
+            for (int i = 0; i < tcl.size(); i++) {
+                std::cout << tcl[i] << std::endl;
+            }
+            Sleep(50);
+            std::cout << "saimos do loop" << std::endl;
+    
+            h1->InputHK(tcl);
             break;
         }
         else {
